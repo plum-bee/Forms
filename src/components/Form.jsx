25b31c8cced5be8e9698fac1f4ac56bd2ada1d8f
gdg2004396 <113702199+gdg2004396@users.noreply.github.com>
2023-09-27 10:12:43 +0200

    DNI VALIDATOR

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,104 +1,128 @@
-import React from 'react'
-import useForm from '../hooks/useForm'
-import InputField from './InputField'
-import SelectField from './SelectField'
+import React from 'react';
+import useForm from '../hooks/useForm';
+import InputField from './InputField';
+import SelectField from './SelectField';
 
-function Form () {
-  const initialData = {
-    name: '',
-    surname: '',
-    username: '',
-    country: '',
-    dni: ''
-  }
-  const availableCountries = ['SPAIN', 'ARGENTINA']
+function Form() {
+    const initialData = {
+        name: '',
+        surname: '',
+        username: '',
+        country: '',
+        dni: ''
+    };
 
-  const validate = formData => {
-    const errors = {}
+    const availableCountries = ['SPAIN', 'ARGENTINA'];
 
-    for (let fieldId in formData) {
-      if (formData[fieldId].trim() === '') {
-        errors[fieldId] = `${fieldId} field is required `
-      } else if (formData[fieldId] !== formData[fieldId].toUpperCase()) {
-        errors[fieldId] = `${fieldId} must be in uppercase `
-      }
-    }
+    const validateDniSpain = dni => {
+        const letterArray = "TRWAGMYFPDXBNJZSQVHLCKET";
+        const regExPattern = /^\d{8}[A-Z]$/;
 
-    if (formData.username.includes(formData.name) && formData.name !== '') {
-      errors.username = 'username cannot contain name'
-    }
+        if (!regExPattern.test(dni)) return false;
 
-    if (formData.username.length > 10) {
-      errors.username = 'username cannot have more than 10 chars'
-    }
+        const char = dni.charAt(8);
+        const number = dni.substr(0, 8);
+        return char === letterArray.charAt(number % 23);
+    };
 
-    return errors
-  }
+    const validateDniArgentina = dni => {
+        const regExPattern = /^\d{7,8}$/;
+        return regExPattern.test(dni);
+    };
 
-  const { formData, formErrors, handleChange, handleBlur } = useForm(
-    initialData,
-    validate
-  )
+    const validate = formData => {
+        const errors = {};
 
-  return (
-    <form>
-      <InputField
-        label='Name'
-        id='name'
-        name='name'
-        placeholder='Enter your name'
-        value={formData.name}
-        handleChange={handleChange}
-        handleBlur={handleBlur}
-        error={formErrors.name}
-      />
+        for (let fieldId in formData) {
+            if (formData[fieldId].trim() === '') {
+                errors[fieldId] = `${fieldId} field is required `;
+            } else if (formData[fieldId] !== formData[fieldId].toUpperCase()) {
+                errors[fieldId] = `${fieldId} must be in uppercase `;
+            }
+        }
 
-      <InputField
-        label='Surname'
-        id='surname'
-        name='surname'
-        placeholder='Enter your surname'
-        value={formData.surname}
-        handleChange={handleChange}
-        handleBlur={handleBlur}
-        error={formErrors.surname}
-      />
+        if (formData.username.includes(formData.name) && formData.name !== '') {
+            errors.username = 'username cannot contain name';
+        }
 
-      <InputField
-        label='Username'
-        id='username'
-        name='username'
-        placeholder='Enter your username'
-        value={formData.username}
-        handleChange={handleChange}
-        handleBlur={handleBlur}
-        error={formErrors.username}
-      />
+        if (formData.username.length > 10) {
+            errors.username = 'username cannot have more than 10 chars';
+        }
 
-      <SelectField
-        label='Country'
-        id='country'
-        name='country'
-        placeholder='Select your country'
-        value={formData.country}
-        options={availableCountries}
-        handleChange={handleChange}
-        handleBlur={handleBlur}
-        error={formErrors.country}
-      />
+        // Validación del DNI
+        if (formData.country === 'SPAIN' && !validateDniSpain(formData.dni)) {
+            errors.dni = 'Invalid Spanish DNI';
+        } else if (formData.country === 'ARGENTINA' && !validateDniArgentina(formData.dni)) {
+            errors.dni = 'Invalid Argentinian DNI';
+        }
 
-      <InputField
-        label='DNI'
-        id='dni'
-        name='dni'
-        placeholder='Enter your DNI'
-        value={formData.dni}
-        handleChange={handleChange}
-        handleBlur={handleBlur}
-        error={formErrors.dni}
-      />
-    </form>
-  )
+        return errors;
+    };
+
+    const { formData, formErrors, handleChange, handleBlur } = useForm(
+        initialData,
+        validate
+    );
+
+    return (
+        <form>
+            <InputField
+                label='Name'
+                id='name'
+                name='name'
+                placeholder='Enter your name'
+                value={formData.name}
+                handleChange={handleChange}
+                handleBlur={handleBlur}
+                error={formErrors.name}
+            />
+
+            <InputField
+                label='Surname'
+                id='surname'
+                name='surname'
+                placeholder='Enter your surname'
+                value={formData.surname}
+                handleChange={handleChange}
+                handleBlur={handleBlur}
+                error={formErrors.surname}
+            />
+
+            <InputField
+                label='Username'
+                id='username'
+                name='username'
+                placeholder='Enter your username'
+                value={formData.username}
+                handleChange={handleChange}
+                handleBlur={handleBlur}
+                error={formErrors.username}
+            />
+
+            <SelectField
+                label='Country'
+                id='country'
+                name='country'
+                placeholder='Select your country'
+                value={formData.country}
+                options={availableCountries}
+                handleChange={handleChange}
+                handleBlur={handleBlur}
+                error={formErrors.country}
+            />
+
+            <InputField
+                label='DNI'
+                id='dni'
+                name='dni'
+                placeholder='Enter your DNI'
+                value={formData.dni}
+                handleChange={handleChange}
+                handleBlur={handleBlur}
+                error={formErrors.dni}
+            />
+        </form>
+    );
 }
 
-export default Form
+export default Form;
